9a03867fb410177bf6fe25ec67160c2af95b080a
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-09-02 03:19:04 -0500

    Se crearon los metodos y vistas para los Equipos de laboratorio.

---
+++ AplicacionReservas/Controllers/EquiposController.cs
@@ -0,0 +1,122 @@
+﻿using AplicacionReservas.Data;
+using AplicacionReservas.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AplicacionReservas.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EquiposController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Equipos
+        public async Task<IActionResult> Index()
+        {
+            var equipos = await _context.Equipos.ToListAsync();
+            return View(equipos);
+        }
+
+        // GET: Equipos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var equipo = await _context.Equipos.FirstOrDefaultAsync(e => e.Id == id);
+            if (equipo == null) return NotFound();
+
+            return View(equipo);
+        }
+
+        // GET: Equipos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Equipos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Equipos");
+            }
+            return View(equipo);
+        }
+
+        // GET: Equipos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var equipo = await _context.Equipos.FindAsync(id);
+            if (equipo == null) return NotFound();
+
+            return View(equipo);
+        }
+
+        // POST: Equipos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Equipo equipo)
+        {
+            if (id != equipo.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipoExists(equipo.Id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction("Index", "Equipos");
+            }
+            return View(equipo);
+        }
+
+        // GET: Equipos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var equipo = await _context.Equipos.FirstOrDefaultAsync(e => e.Id == id);
+            if (equipo == null) return NotFound();
+
+            return View(equipo);
+        }
+
+        // POST: Equipos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var equipo = await _context.Equipos.FindAsync(id);
+            if (equipo != null)
+            {
+                _context.Equipos.Remove(equipo);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", "Equipos");
+        }
+
+        private bool EquipoExists(int id)
+        {
+            return _context.Equipos.Any(e => e.Id == id);
+        }
+    }
+}
